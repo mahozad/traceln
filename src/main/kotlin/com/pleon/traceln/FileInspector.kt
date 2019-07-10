6a5566ebff16eb28761c9a0135dafe9cf8c0afa5
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2019-07-10 19:22:45 +0430

    Integrate LineCounter class to inspector; Use a new library for detecting file type; Rename some classes;

--- src/main/kotlin/com/pleon/traceln/FileInspector.kt
+++
@@ -1,9 +0,0 @@
-package com.pleon.traceln
-
-import java.io.File
-import java.nio.file.Files
-
-class FileInspector {
-
-    fun isTextFile(file: File) = Files.probeContentType(file.toPath()).contains("text")
-}
