01bc364745989a5937aee50edd2a5ff32fd83b88
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2019-07-23 18:39:35 +0430

    Inline path variable

--- src/main/kotlin/com/pleon/traceln/Inspector.kt
+++ src/main/kotlin/com/pleon/traceln/Inspector.kt
@@ -6,16 +6,14 @@ import java.nio.file.Files
 import java.nio.file.Path
 import kotlin.streams.toList
 
-private val typesPath = Path.of(Adder::class.java.getResource("/types.txt").toURI())
-
 class Inspector {
 
-    private val language = Files.lines(typesPath).toList()
-        .associateBy({ it.substringBefore(':') }, { it.substringAfter(':') })
+    private val type = Files.lines(Path.of(javaClass.getResource("/types.txt").toURI()))
+        .toList().associateBy({ it.substringBefore(':') }, { it.substringAfter(':') })
 
     fun isTextFile(file: File) = getMagicMatch(file, true).mimeType.contains("text")
 
     fun countLines(file: File) = file.readLines().size
 
-    fun getType(file: File) = language.getOrDefault(file.name.substringAfter('.'), "Other")
+    fun getType(file: File) = type.getOrDefault(file.name.substringAfter('.'), "Other")
 }
