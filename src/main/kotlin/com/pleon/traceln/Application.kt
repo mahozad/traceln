a64244e4d76b0baa64eb5a2a7124dc5c47decbd4
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2019-07-24 13:02:17 +0430

    Update a comment

--- src/main/kotlin/com/pleon/traceln/Application.kt
+++ src/main/kotlin/com/pleon/traceln/Application.kt
@@ -2,9 +2,9 @@ package com.pleon.traceln
 
 import java.nio.file.Path
 
-// For list of official Github languages and their colors see:
-//  [https://github.com/github/linguist/blob/master/lib/linguist/languages.yml]
-// For how Github language detection works see: [https://github.com/github/linguist]
+// For list of official Github languages and their colors see [https://github.com/ozh/github-colors]
+//     and [https://github.com/github/linguist/blob/master/lib/linguist/languages.yml]
+// For how Github language detection works see [https://github.com/github/linguist]
 // And for using Github REST api see [https://developer.github.com/v3/repos/]
 
 fun main(args: Array<String>) {
