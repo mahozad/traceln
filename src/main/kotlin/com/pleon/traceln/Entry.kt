db08c6aa95a5f2b639cbc505abc3daf43ae76b89
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2019-08-07 15:39:06 +0430

    Update toString() function

--- src/main/kotlin/com/pleon/traceln/Entry.kt
+++ src/main/kotlin/com/pleon/traceln/Entry.kt
@@ -1,3 +1,5 @@
 package com.pleon.traceln
 
-data class Entry(val name: String, val lines: Int, val percentage: Double)
+data class Entry(val name: String, val lines: Int, val percentage: Double) {
+    override fun toString() = "Type: $name\tLines: $lines\tPercentage: $percentage\n"
+}
