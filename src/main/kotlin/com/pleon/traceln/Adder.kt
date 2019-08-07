fb3693d8ee7ccf15a9b1183b9a903620c97eee95
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2019-08-07 15:39:16 +0430

    Add minor refactorings

--- src/main/kotlin/com/pleon/traceln/Adder.kt
+++ src/main/kotlin/com/pleon/traceln/Adder.kt
@@ -2,13 +2,12 @@ package com.pleon.traceln
 
 class Adder {
 
-    private var totalLines = 0.0
     private val lineCount = mutableMapOf<String, Int>()
+    private var totalLines = 0.0
 
     fun getResult() = lineCount.map { Entry(it.key, it.value, it.value / totalLines * 100) }
 
     fun add(type: String, lines: Int) {
-        totalLines += lines
-        lineCount.merge(type, lines, Math::addExact)
+        lineCount.merge(type, lines, Math::addExact).also { totalLines += lines }
     }
 }
