bda0accb187a30114f6d4f2c7d5560b4b26ea9aa
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2019-09-28 16:32:18 +0330

    Add javafx.swing module

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,8 +23,7 @@ javafx {
     version = "13"
     modules(
         "javafx.base", "javafx.controls", "javafx.fxml",
-        "javafx.graphics", "javafx.media", "javafx.web"
-        /*, "javafx.swing", "javafx.swt"*/
+        "javafx.graphics", "javafx.media", "javafx.web", "javafx.swing"
     )
 }
 
