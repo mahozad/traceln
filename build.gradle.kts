e7e6800240f3235fea544f41e62bed9027061593
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2019-09-28 16:10:21 +0330

    Use default kotlin() dependencies; Update AssertJ library; Add a comment

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,12 +43,14 @@ application {
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation("net.sf.jmimemagic:jmimemagic:0.1.5")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5:1.3.40")
+    testImplementation(kotlin("test"))
+    testImplementation(kotlin("test-junit5"))
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.5.0")
-    testImplementation("org.assertj:assertj-core:3.12.2")
+    testImplementation("org.assertj:assertj-core:3.13.2")
     testImplementation("io.mockk:mockk:1.9.3")
 }
 
 tasks.withType<KotlinCompile> {
+    // Target version of the generated JVM bytecode (1.6, 1.8, 9, 10, 11 or 12), default is 1.6
     kotlinOptions.jvmTarget = "12"
 }
