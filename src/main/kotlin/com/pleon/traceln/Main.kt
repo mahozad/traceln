2b8ffa03c600f482ac522543d53720d468b0041d
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2019-09-30 19:33:41 +0330

    Add a temporary webview chart

--- src/main/kotlin/com/pleon/traceln/Main.kt
+++ src/main/kotlin/com/pleon/traceln/Main.kt
@@ -4,6 +4,7 @@ import javafx.application.Application
 import javafx.fxml.FXMLLoader
 import javafx.scene.Parent
 import javafx.scene.Scene
+import javafx.scene.web.WebView
 import javafx.stage.Stage
 
 // For list of official Github languages and their colors see [https://github.com/ozh/github-colors]
@@ -16,10 +17,16 @@ fun main(args: Array<String>) {
 }
 
 class Main : Application() {
+
     override fun start(primaryStage: Stage) {
         val root: Parent = FXMLLoader.load(javaClass.getResource("/fxml/scene-main.fxml"))
         primaryStage.title = "Traceln"
         primaryStage.scene = Scene(root, 300.0, 275.0)
+
+        val url = javaClass.getResource("/html/charts.html").toExternalForm()
+        (root.lookup("WebView") as WebView).engine.load(url)
+
         primaryStage.show()
     }
+
 }
